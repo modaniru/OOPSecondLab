deb9347a1cb0aedcef86b9040aa1294271572bff
aleksejmoksancev <mokshantsev.aleksei>
2023-02-24 13:55:15 +0500

    add main2.cpp file and some classes in main.cpp

--- main.cpp
+++ main.cpp
@@ -1,6 +1,56 @@
-#include <iostream>
-
-int main() {
-    std::cout << "Hello, World!" << std::endl;
-    return 0;
-}
+//#include <iostream>
+//
+//class Animal{
+//public:
+//    int age;
+//    void eat(){
+//        printf("eat()\n");
+//    }
+//    Animal(){
+//        printf("Animal()\n");
+//        age = 0;
+//        name = "name";
+//        isSick = false;
+//    }
+//    Animal(int age, std::string name, bool isSick){
+//        printf("Animal(int age, std::string name, bool isSick)\n");
+//        this->age = age;
+//        this->name = name;
+//        this->isSick = isSick;
+//    }
+//    Animal(const Animal& animal){
+//        printf("Animal(const Animal& animal)\n");
+//        this->age = animal.age;
+//        this->isSick = animal.isSick;
+//        this->name = animal.name;
+//    }
+//    ~Animal(){
+//        printf("~Animal()\n");
+//        printf("\tage = %d\n", age);
+//        printf("\tname = %s\n", name.c_str());
+//        printf("\tisSick = %d\n", isSick);
+//    }
+//    bool isSick_();
+//private:
+//    std::string name;
+//protected:
+//    bool isSick;
+//};
+//
+//bool Animal::isSick_(){
+//    printf("Animal::isSick_()\n");
+//    return isSick;
+//}
+//
+//int main() {
+//    {
+//        Animal animal;
+//        Animal animal1(1, "name", true);
+//        Animal animal2(animal1);
+//        animal.age = 2;
+//        //animal.name
+//        //animal.isSick не могу из-за модификатора доступа private и protected
+//        animal.isSick_();
+//        animal1.eat();
+//    }
+//}
